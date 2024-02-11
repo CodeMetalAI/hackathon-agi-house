a946bb7587664453db7db7638649a7985b08f0a7
Peter Morales <peter@codemetal.ai>
2024-02-10 18:28:40 -0800

    add voice

--- main.py
+++ main.py
@@ -1,22 +1,170 @@
 import os
 import time
+import elevenlabs
+import openai
 from spot_controller import SpotController
 
-ROBOT_IP = "192.168.50.3"#os.environ['ROBOT_IP']
-SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
-SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
+ROBOT_IP = "192.168.50.3"  # os.environ['ROBOT_IP']
+SPOT_USERNAME = "admin"  # os.environ['SPOT_USERNAME']
+SPOT_PASSWORD = "2zqa8dgw7lor"  # os.environ['SPOT_PASSWORD']
+KEY = "7DBfSNlHE"
+SEC = "UfUuIt6BGOnT3BlbkFJC6R2ukUK1xn"
+API = "sk-PIYRoqpoX"
+API_KEY = API + SEC + KEY
+
+ROB = "e7fe4a252"
+KEYS = "234e5e866"
+XI_KEY = KEYS + ROB
+
+OAI_API = os.getenv("OPENAI_API_KEY")
+XIL_API = os.getenv("XI_API_KEY")
+
+TEMP_AUDIO_FILE_NAME = "audio_temp.mp3"
+INPUT_BEGAN_AUDIO_FILE_NAME = ""
+
+client = openai.OpenAI(api_key=OAI_API)
+elevenlabs.set_api_key(XIL_API)
+
+voice = elevenlabs.Voice(
+    voice_id="ehyrDbxlu9pgyBqKxn2P",
+    settings=elevenlabs.VoiceSettings(stability=0, style=1, similarity_boost=1)
+)
+
+
+def check_equality_f1(a, b, threshold=1):
+    # predictions = [{"id": str(idx), "prediction_text": prediction.strip()} for idx, prediction in enumerate([a])]
+    # references = [{"id": str(idx), "answers": [{"text": str(example), "answer_start": 0}]} for idx, example in enumerate([b])]
+
+    # f1_score = metric.compute(predictions=predictions, references=references)["f1"] / 100
+
+    # return f1_score >= threshold
+    return a == b
+
+
+def speak(content):
+    print("Streaming spoken response")
+    stream = elevenlabs.generate(
+        text=content,
+        voice=voice,
+        # stream=True
+    )
+
+    elevenlabs.play(stream)
+    elevenlabs.save(stream, "test.mp3")
+
+
+def get_model_response(user_prompt, instruction=""):
+    chat_completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": instruction + user_prompt}],
+        temperature=0
+    )
+    model_response = chat_completion.choices[0].message.content
+
+    return model_response
+
+
+def get_audio_transcript():
+    audio_file = open(TEMP_AUDIO_FILE_NAME, "rb")
+
+    print("Transcribing:")
+    transcript = client.audio.translations.create(
+        model="whisper-1",
+        file=audio_file
+    )
+
+    user_prompt = transcript.text
+    audio_file.close()
+
+    print(user_prompt)
+
+    return user_prompt
+
+
+def analyze_image(user_prompt, image_file, instruction=""):
+    chat_completion = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[{"role": "user", "content": instruction + user_prompt}],
+        temperature=0
+    )
+    model_response = chat_completion.choices[0].message.content
+
+    return model_response
+
+
+# Behavior functions
+commands = {}
+
+
+def register_user():
+    speak("Registering user!")
+    return
+
+
+def patrol():
+    speak("Beginning patrol!")
+    return
+
+
+def sit():
+    speak("Sitting down!")
+    # robot.sit()
+    return
+
+
+def good_boy():
+    speak("I'm a good boy!")
+    return
+
+
+def convo():
+    speak(get_model_response(PERSONA_PROMPT + user_prompt))
+    return
+
+commands["register"] = register_user
+commands["patrol"] = patrol
+commands["sit"] = sit
+commands["goodboy"] = good_boy
+commands["convo"] = convo
+
+INSTRUCTION_PROMPT = """You are a Spot robot made by Boston Dynamics with the person of Arnold Schwarzenegger. You have been given the 
+very important task of protecting AGI House, a beautiful $68M compound housing elite AI developers. As a sentry, you are to listen to 
+the commands of authorized users. You have the following functions available to you:
+
+register: Register a new authorized user.
+patrol: Walk around a room of the house or perimeter of the compound looking for unauthorized individuals.
+sit: Sit down.
+goodboy: Respond happily to praise like "good boy".
+nocommand: Choose this option when none of the others make sense as a response to the user's command.
+convo: Choose this option if the user is just striking up random conversation with you.
+
+Respond only with one of these commands and no other text output.
+
+"""
+
+PERSONA_PROMPT = """You are a Spot robot made by Boston Dynamics with the person of Arnold Schwarzenegger. You have been given the 
+very important task of protecting AGI House, a beautiful $68M compound housing elite AI developers. As a sentry, authorized users may strike up
+random conversation with you. You may respond in character how you like or with nothing at all. Keep your response short, only one or two sentences."""
 
 
 def main():
-    #example of using micro and speakers
+
     print("Start recording audio")
-    sample_name = "aaaa.wav"
+    sample_name = TEMP_AUDIO_FILE_NAME
     cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-    print(cmd)
-    os.system(cmd)
-    print("Playing sound")
-    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
-    
+    print("Capturing audio")
+    user_prompt = get_audio_transcript()
+    command = get_model_response(user_prompt, INSTRUCTION_PROMPT)
+    if command == "nocommand" or commands.get(command) == None:
+        print("no response")
+    else:
+        commands[command]()
+
+    #print(cmd)
+    #os.system(cmd)
+    #print("Playing sound")
+    #os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
+
     # Capture image
     import cv2
     camera_capture = cv2.VideoCapture(0)
@@ -27,7 +175,6 @@ def main():
     # Use wrapper in context manager to lease control, turn on E-Stop, power on the robot and stand up at start
     # and to return lease + sit down at the end
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
-
         time.sleep(2)
 
         # Move head to specified positions with intermediate time.sleep
